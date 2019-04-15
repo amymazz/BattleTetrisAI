2bebdad1e3f44e002579bc14028afd29496e22d0
AmyMazz <amazzucotelli@gmail.com>
2019-04-15 16:05:25 -0400

    add garbage rows

--- GameBoard.py
+++ GameBoard.py
@@ -1,4 +1,5 @@
 # GameBoard.py
+import random
 
 class GameBoard:
     def __init__(self):
@@ -7,6 +8,8 @@ class GameBoard:
         self._board = [[0 for j in range(self._width)] for i in range(self._height)]
         self._total_cells = self._width * self._height
         self._completed_rows = []  # marks rows that can be removed
+        self._double_hole = False  # for adding garbage lines
+        self._game_over = False  # set if blocks pushed over the top
 
     def __repr__(self):
         repr = "\n"
@@ -67,12 +70,36 @@ class GameBoard:
 
         return completed
 
-    def clear_rows(self):
+    def clear_completed_rows(self):
         """ Clears completed rows and moves everything above down """
         for row in self._completed_rows:
             self._board.pop(row)
             self._board.append([0 for j in range(self._width)])
         self._completed_rows = []
+
+    def add_garbage_row(self):
+        """ Adds garbage row from opponent """
+        newRow = [1 for j in range(self._width)]
+
+        h = [ None, None ]
+        for i in range(len(h)):
+            if h[0] is None:
+                h[0] = random.randint(0, self._width-1)
+                newRow[h[0]] = 0
+            elif self._double_hole:
+                h[1] = h[0]
+                while h[1] == h[0]:
+                    h[1] = random.randint(0, self._width-1)
+                newRow[h[1]] = 0
+
+        self._board = [newRow] + self._board
+        self._double_hole = False if self._double_hole else True
+        top = self._board.pop()
+
+        for i in range(len(top)):  # check if a block was pushed over the top
+            if top[i] > 0:
+                self._game_over = True
+
 
     def temp_add_piece(self, piece):
         """ Function to add arbitrary pieces to the board, for testing only """
@@ -90,9 +117,14 @@ if __name__ == '__main__':
     print("Occupied cells: {}".format(tetrisBoard.occupied_cells()))
     print("Pile Height: {}".format(tetrisBoard.pile_height()))
     print("Holes: {}".format(tetrisBoard.num_holes()))
-    tetrisBoard.clear_rows() # this should not error
+    tetrisBoard.clear_completed_rows() # this should not error
 
     tetrisBoard.temp_add_piece([(3,0), (4,0), (5,0), (6,0), (7,0), (8,0), (9,0)])
     print("Completed Rows: {}".format(tetrisBoard.num_completed_rows()))
-    tetrisBoard.clear_rows()
+    tetrisBoard.clear_completed_rows()
+    print(tetrisBoard)
+
+    tetrisBoard.add_garbage_row()
+    tetrisBoard.add_garbage_row()
+    tetrisBoard.add_garbage_row()
     print(tetrisBoard)
